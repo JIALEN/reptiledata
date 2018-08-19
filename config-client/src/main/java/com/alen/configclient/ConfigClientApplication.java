21a110a3123b84f84a061b122f9f5fe338612aa4
lijialun <lijialun6481@dingtalk.com>
2018-08-19 10:52:13 +0800

    sw

--- config-client/src/main/java/com/alen/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/alen/configclient/ConfigClientApplication.java
@@ -2,8 +2,14 @@ package com.alen.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//通过注解@EnableEurekaClient 表明自己是一个eurekaclient.
+@EnableEurekaClient
+@ComponentScan(basePackages = "com.alen.configclient")
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
---
+++ config-client/src/main/java/com/alen/configclient/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.alen.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author alen
+ * @create 2018-08-18 12:20
+ **/
+@RestController
+public class HelloController {
+    @Value("${name}")
+    private String name;
+    @Value("${democonfigclient.message}")
+    private String message;
+    /**
+     * 从配置中心读取的变量的值
+     * @param
+     * @return
+     */
+    @RequestMapping("/hello")
+    public String getName() {
+        System.out.println(name);
+        System.out.println(message);
+        return "";
+    }
+}
